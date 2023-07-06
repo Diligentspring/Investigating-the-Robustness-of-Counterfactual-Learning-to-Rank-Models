07308c674ee124d279ce67ba256484d94683ad79
nzc <niuzechun@ruc.edu.cn>
2023-07-06 11:58:48 -0400

    update

--- click_simulator.py
+++ click_simulator.py
@@ -131,8 +131,9 @@ class DependentClickModel(ClickModel):
         click_p = self.click_prob[relevance_label if relevance_label < len(
             self.click_prob) else -1]
         if random.random() < self.epsilon:
-            click_p = 1.0
-        click = 1.0 if random.random() < exam_p * click_p else 0.0
+            click = 1.0
+        else:
+            click = 1.0 if random.random() < click_p else 0.0
         return click, cont_p
 
     def getExamProb(self, rank):
